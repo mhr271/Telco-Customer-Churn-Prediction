76350324dc2fe3f9fe044d3672531ec8137c055a
mhr271 <humaidirahman271@gmail.com>
2026-07-15 18:25:57 +1000

    Import libraries for data processing and modeling
    Added necessary imports for data processing and model training.

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
+from xgboost import XGBClassifier
 
